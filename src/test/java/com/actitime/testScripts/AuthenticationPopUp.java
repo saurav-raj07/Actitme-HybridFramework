2c81544d535c3f9fcb5cc4ae6ac86f21286625e0
saurav-raj07 <saurav.raj5338@gmail.com>
2019-06-27 01:13:30 +0530

    second commit

--- src/test/java/com/actitime/testScripts/AuthenticationPopUp.java
+++ src/test/java/com/actitime/testScripts/AuthenticationPopUp.java
@@ -12,13 +12,6 @@ public class AuthenticationPopUp extends BaseLib {
 	public void authentationPopUptesting() {
 		
 		
-//		System.setProperty("webdriver.chrome.driver", "./exefiles/chromedriver.exe");
-////		WebDriver driver = new InternetExplorerDriver();
-////		WebDriver driver = new FirefoxDriver();
-//		WebDriver driver =new ChromeDriver();
-//		driver.manage().window().maximize();
-//		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-//		driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
 		String innertextOfElement = driver.findElement(By.xpath("//h3[text()='Basic Auth']")).getAttribute("innerText");
 		System.out.println(innertextOfElement);
 		
@@ -36,8 +29,8 @@ public class AuthenticationPopUp extends BaseLib {
 	
 	@Test(enabled=true)
 	public void sikuliFileDownload() throws Throwable {
-		System.out.println(" hello world");
-		Screen screen = new Screen();
+		System.out.println(" >>>> hello world <<<<");
+//		Screen screen = new Screen();
 //		
 //		Pattern pattern = new Pattern("abc");
 //		
